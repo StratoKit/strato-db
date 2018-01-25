0962ea2e57f163cc5bc9a7fd3628f1d4d3e7be6e
Wout Mertens <Wout.Mertens@gmail.com>
2018-01-25 14:03:57 +0100

    slugify: add randomString(n), replaces uuid
    slugifyString now accepts parameter to always return something

--- src/slugify.js
+++ src/slugify.js
@@ -1,10 +1,20 @@
 import {deburr} from 'lodash'
-import uuid from 'uuid'
 
-export const slugifyString = name => {
+const abc = 'abcdefghijklmnopqrstuvwxyz0123456789'
+export const randomString = n =>
+	Array.apply(null, new Array(n))
+		.map(() => {
+			return abc.charAt(Math.floor(Math.random() * abc.length))
+		})
+		.join('')
+
+export const slugifyString = (name, alwaysResult) => {
 	// extract name from i18n objects
 	const t = typeof name === 'string' ? name : name && name[Object.keys(name)[0]]
-	if (!t) throw new Error(`Cannot slugify ${name}`)
+	if (!t) {
+		if (alwaysResult) return randomString(12)
+		throw new Error(`Cannot slugify ${name}`)
+	}
 	return encodeURIComponent(deburr(t).trim())
 		.replace(/(%..|[()])/g, '-')
 		.replace(/--+/g, '-')
@@ -13,10 +23,9 @@ export const slugifyString = name => {
 		.toLowerCase()
 }
 
-// This is not race-safe - only use for write-seldomn things like backoffice
+// This is not race-safe - only use for write-seldomn things like backoffice or inside transactions
 export const uniqueSlugId = async (model, name, colName) => {
-	const slug = slugifyString(name)
-	if (!slug) return uuid.v1()
+	const slug = slugifyString(name, true)
 	let id = slug
 	let i = 1
 	// eslint-disable-next-line no-await-in-loop
