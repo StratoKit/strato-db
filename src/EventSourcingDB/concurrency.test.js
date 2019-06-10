5e369c2b867cc79c94091187a117c1b287d0e33c
Wout Mertens <Wout.Mertens@gmail.com>
2019-06-10 14:47:06 +0200

    ESDB: other DB conns stable during transaction

--- src/EventSourcingDB/concurrency.test.js
+++ src/EventSourcingDB/concurrency.test.js
@@ -2,6 +2,8 @@ import sysPath from 'path'
 import tmp from 'tmp-promise'
 import ESDB from '.'
 
+let resolveMe, waitP
+
 const testModels = {
 	subber: {
 		preprocessor: async ({model, store, event}) => {
@@ -18,6 +20,21 @@ const testModels = {
 					expect(await model.get('hey')).toBeTruthy()
 					break
 				default:
+			}
+		},
+	},
+	waiter: {
+		reducer: async (model, {type, data}) => {
+			if (type === 'waiter') {
+				return {ins: [data]}
+			}
+		},
+		deriver: async ({event}) => {
+			if (event.type === 'waiter') {
+				resolveMe()
+				resolveMe = null
+				await waitP
+				waitP = null
 			}
 		},
 	},
@@ -61,7 +78,28 @@ test('multiple ESDB', async () => {
 test('subevent handlers see intermediate state', async () => {
 	await db1._dispatchWithError('main')
 })
-// TODO verify that db doesn't see transaction changes in rwDb (use await in reducer)
+
+test(`RO and other DB don't see transaction`, async () => {
+	const firstP = new Promise(resolve => {
+		resolveMe = resolve
+	})
+	let resolveSecond
+	waitP = new Promise(resolve => {
+		resolveSecond = resolve
+	})
+	const eventP = db1.dispatch('waiter', {id: 'w'})
+	await firstP
+	const v = await db1.getVersion()
+	expect(await db1.rwStore.waiter.get('w')).toBeTruthy()
+	expect(await db1.store.waiter.get('w')).toBeFalsy()
+	expect(await db1.getVersion()).toBe(v)
+	expect(await db2.rwStore.waiter.get('w')).toBeFalsy()
+	expect(await db2.getVersion()).toBe(v)
+	resolveSecond()
+	const {v: v2} = await eventP
+	expect(await db2.store.waiter.get('w')).toBeTruthy()
+	await expect(v2).toBeGreaterThan(v)
+})
 // TODO getNextId should only work during transaction, should be separate concept, per-transaction state
 // TODO 10 simulteneous opens of existing db file
 // TODO 10 simulteneous opens of new db file/new queue file with >1 version
