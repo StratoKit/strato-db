26ed8a94f8fc306f1e469d2cf9acb9b181d75583
Wout Mertens <Wout.Mertens@gmail.com>
2020-04-28 17:04:11 +0200

    eslint: use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,56 +15,35 @@ module.exports = {
 		'plugin:promise/recommended',
 		'plugin:unicorn/recommended',
 		'xo/esnext',
+		'plugin:prettier/recommended',
 	],
 	rules: {
-		// the good stuff
-		'constructor-super': 1,
-		'import/first': 1,
-		// Would make this an error but it can't handle * exports
-		'import/named': 1,
-		// our webpack nodeRequire trick is not a package
-		'import/no-unresolved': [2, {ignore: ['nodeRequire']}],
-		'no-const-assign': 1,
-		'no-console': 2,
+		// !! is nice
 		'no-implicit-coercion': [2, {allow: ['!!']}],
-		'no-this-before-super': 1,
-		'no-unreachable': 1,
-		'valid-typeof': 1,
+		// == and != are nice for null+undefined
 		eqeqeq: [2, 'allow-null'],
+		// allow unused vars starting with _
+		'no-unused-vars': [
+			'error',
+			{varsIgnorePattern: '^_', argsIgnorePattern: '^_'},
+		],
+		// we want a clean console - eslint-disable every wanted one
+		'no-console': 2,
+		'object-shorthand': 2,
+		// too many false positives
+		'require-atomic-updates': 1,
 
-		// ignore everything handled by prettier
-		'arrow-parens': 0,
-		'babel/arrow-parens': 0,
-		'brace-style': 0,
 		'capitalized-comments': 0,
-		'comma-dangle': 0,
-		'function-paren-newline': 0,
-		'max-statements-per-line': 0,
 		'no-eq-null': 0,
-		'no-extra-semi': 0,
-		'no-mixed-operators': 0,
-		'no-mixed-spaces-and-tabs': 0,
-		'no-multi-spaces': 0,
-		'no-trailing-spaces': 0,
-		'object-curly-spacing': 0,
 		'one-var': 0,
-		'operator-linebreak': 0,
-		'padded-blocks': 0,
 		'padding-line-between-statements': 0,
 		'prefer-template': 0,
 		'promise/param-names': 0,
-		'quote-props': 0,
-		'space-before-function-paren': 0,
-		'space-in-parens': 0,
-		'space-infix-ops': 0,
 		'unicorn/catch-error-name': 0,
 		'unicorn/explicit-length-check': 0,
 		'unicorn/filename-case': 0,
 		'unicorn/no-nested-ternary': 0,
+		'unicorn/no-null': 0,
 		'unicorn/prevent-abbreviations': 0,
-		curly: 0,
-		indent: 0,
-		quotes: 0,
-		semi: 0,
 	},
 }
