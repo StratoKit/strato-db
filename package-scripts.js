8252dd555af63b1ab3cb737af600168355d5608a
Wout Mertens <Wout.Mertens@gmail.com>
2020-05-14 11:38:40 +0200

    types: generate types.d.ts in npm package

--- package-scripts.js
+++ package-scripts.js
@@ -11,7 +11,7 @@ try {
 const runBabel = `NODE_ENV=production babel -s true --ignore '**/*.test.js,**/__snapshots__' -d dist/`
 const scripts = {
 	build: {
-		default: `nps build.clean build.babel build.doc`,
+		default: `nps build.clean build.babel build.doc build.types`,
 		clean: rimraf('dist/'),
 		babel: `${runBabel} src/`,
 		watch: `${runBabel} --watch src/`,
@@ -23,6 +23,11 @@ const scripts = {
 			`echo >> API.md`,
 			`git log -n1 --format=format:"_Generated from %H, %cI_" >> API.md`,
 			`prettier --write API.md`
+		),
+		types: series(
+			`jsdoc -t node_modules/tsd-jsdoc/dist -r src -d dist`,
+			// https://github.com/englercj/tsd-jsdoc/issues/64
+			`echo 'export const DB: DB; export const EventSourcingDB: EventSourcingDB; export const SQLite: SQLite; export const EventQueue: EventQueue; export const applyResult: applyResult; export const ESModel: ESModel; export const JsonModel: JsonModel;' >> dist/types.d.ts`
 		),
 	},
 	test: {
