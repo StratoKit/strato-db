94f01dcb344121094d3c46363d2149aad7ad5ede
Wout Mertens <Wout.Mertens@gmail.com>
2023-07-19 10:36:31 +0200

    direnv: setup with Nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev environment for strato-db";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+
+          nativeBuildInputs = with pkgs; [
+            sqlite-interactive
+
+            nodejs-18_x
+          ];
+        };
+      });
+}
