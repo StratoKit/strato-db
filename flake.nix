40a55e7a59348fd65c4b2ec744c1b6facf1c21cd
Wout Mertens <Wout.Mertens@gmail.com>
2024-05-15 08:27:15 +0200

    dep updates

--- flake.nix
+++ flake.nix
@@ -1,21 +1,38 @@
 {
   description = "Dev environment for strato-db";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShell = pkgs.mkShell {
+  inputs = {
+    # Make sure to use the same locked commits as the nix-infra deploys
+    # That way the packages are shared
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-          nativeBuildInputs = with pkgs; [
-            sqlite-interactive
+  outputs = { self, nixpkgs }:
+    let
+      b = builtins;
+      # Make sure that this include runtime libs linked by npm builds
+      deps = pkgs: with pkgs; [
+        bashInteractive
+        sqlite-interactive
+        # NodeJS
+        nodejs_22
+        corepack_22
+      ];
+      makeDevShell = system: pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = (deps pkgs) ++ (with pkgs; [
+            gitMinimal
 
-            nodejs-18_x
-          ];
+            # sqlite3 module
+            sqlite-interactive.dev
+          ]);
+          shellHook = ''
+            export PATH=$PWD/node_modules/.bin:$PATH
+          '';
         };
-      });
+      };
+    in
+    {
+      devShells = b.mapAttrs (makeDevShell) nixpkgs.legacyPackages;
+    };
 }
